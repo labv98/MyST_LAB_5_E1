f4abc7975662dc1bf0b9c04903071c7946a6b2ba
danielgarciah <danielgarciah@live.com.mx>
2021-11-16 18:55:13 -0600

    Update

--- functions.py
+++ functions.py
@@ -8,3 +8,23 @@
 # -- repository: YOUR REPOSITORY URL                                                                     -- #
 # -- --------------------------------------------------------------------------------------------------- -- #
 """
+
+import pandas as pd
+import MetaTrader5 as mt5
+
+
+def historicos(path, login, password, server, start_date, end_date, save_name, symbol):
+    connection = mt5.initialize(path=path, login=login, password=password, server=server)
+
+    tuplas = mt5.copy_ticks_range(symbol, start_date, end_date, mt5.TIMEFRAME_H12)
+
+    df = pd.DataFrame(tuplas)
+    df['time'] = pd.to_datetime(df['time'], unit='s')
+    df['time_msc'] = pd.to_datetime(df['time'], unit='ms')
+
+    #
+    # reportpath = path.abspath('ReportesDeals_MT5/') + "/"
+    # save_name = mt5.account_info().name
+    # df.to_excel(reportpath + "Deals_" + save_name + ".xlsx")
+
+    return df
