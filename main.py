d8ab41ccde9a02473566a30f55053f21854dfd9f
danielgarciah <danielgarciah@live.com.mx>
2021-11-28 16:48:52 -0600

    Avance

--- main.py
+++ main.py
@@ -19,16 +19,17 @@ from functions import historicos, moving_average, boolinger_bands, trading
 usuario = 'Daniel Garcia'
 symbol = 'EURUSD'
 meta_path = 'C:\Program Files\MetaTrader 5 Terminal\\terminal64.exe'
-login_count = 5400339 #'Bruno': 5400338 #'Chelsi': 5400342 #'Daniel': 5400339
-password_count = '2qeDQrhu' #'Bruno': 'LHFFV4Nh' #'Chelsi': 'XN1xho9d' #'Daniel': '2qeDQrhu'
+login_count = 5400339 #'Chelsi': 5400342 #'Daniel': 5400339
+password_count = '2qeDQrhu' #'Chelsi': 'XN1xho9d' #'Daniel': '2qeDQrhu'
 server_name = 'FxPro-MT5'
-start_date_train = datetime(year=2018, month=1, day=1)
-end_date_train = datetime(year=2018, month=12, day=31)
-start_date_test = datetime(2019, 2, 1)
-end_date_test = datetime(2020, 2, 1)
+start_date_backtest = datetime(year=2018, month=1, day=1)
+end_date_backtest = datetime(year=2018, month=12, day=31)
+start_date_prueba = datetime(year=2019, month=1, day=1)
+end_date_prueba = datetime(year=2019, month=12, day=31)
 
-datos_train = historicos(meta_path, login_count, password_count, server_name,
-                         start_date_train, end_date_train, usuario, symbol)
+datos_train, datos_prueba = historicos(meta_path, login_count, password_count, server_name,
+                                       start_date_backtest, end_date_backtest, start_date_prueba,
+                                       end_date_prueba, symbol)
 #
 # ma = moving_average(meta_path, login_count, password_count, server_name,
 #                      start_date_train, end_date_train, usuario, symbol)
@@ -40,7 +41,7 @@ datos_train = historicos(meta_path, login_count, password_count, server_name,
 #                    start_date_train, end_date_train, usuario, symbol)
 
 
-print(datos_train)
+print(datos_train, datos_prueba)
 # print(ma)
 # print(bb)
 #print(resultado)
