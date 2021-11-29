875866c830e67bf7ee51b11b8b1b25ce73aafe33
Unknown <lizale_barr@hotmail.com>
2021-11-28 23:00:51 -0600

    Correct 2

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ meta_path = 'C:\Program Files\MetaTrader 5 Terminal\\terminal64.exe'
 login_count = 5400339 #'Chelsi': 5400342 #'Daniel': 5400339
 password_count = '2qeDQrhu' #'Chelsi': 'XN1xho9d' #'Daniel': '2qeDQrhu'
 server_name = 'FxPro-MT5'
-<<<<<<< HEAD
+
 start_date_train = datetime(2018, 1, 1)
 end_date_train = datetime(2019, 1, 1)
 start_date_test = datetime(2019, 2, 1)
@@ -30,7 +30,7 @@ end_date_test = datetime(2020, 2, 1)
 
 datos_train = historicos(meta_path, login_count, password_count, server_name,
                          start_date_train, end_date_train, usuario, symbol)
-=======
+
 start_date_backtest = datetime(year=2018, month=1, day=1)
 end_date_backtest = datetime(year=2018, month=12, day=31)
 start_date_prueba = datetime(year=2019, month=1, day=1)
@@ -39,7 +39,7 @@ end_date_prueba = datetime(year=2019, month=12, day=31)
 datos_train, datos_prueba = historicos(meta_path, login_count, password_count, server_name,
                                        start_date_backtest, end_date_backtest, start_date_prueba,
                                        end_date_prueba, symbol)
->>>>>>> Dani
+
 #
 # ma = moving_average(meta_path, login_count, password_count, server_name,
 #                      start_date_train, end_date_train, usuario, symbol)
@@ -51,11 +51,11 @@ resultado = trading(meta_path, login_count, password_count, server_name,
                     start_date_train, end_date_train, usuario, symbol)
 
 
-<<<<<<< HEAD
+
 # print(datos_train)
-=======
+
 print(datos_train, datos_prueba)
->>>>>>> Dani
+
 # print(ma)
 # print(bb)
 print(resultado)
